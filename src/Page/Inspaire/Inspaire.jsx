5c3767d588bcc4d2a4d4dd63e2f9751f8a331e89
Md. Siam Shekh <siam62349@gmail.com>
2024-07-09 15:56:16 +0600

    maintenance page mark as tour and go tour

--- src/Page/Inspaire/Inspaire.jsx
+++ src/Page/Inspaire/Inspaire.jsx
@@ -1,8 +1,8 @@
 
 const Inspaire = () => {
     return (
-        <div>
-            
+        <div className="flex justify-center items-center text-5xl h-screen w-full">
+            Page is under maintenance
         </div>
     );
 };
--- src/Page/Tour/Tour.jsx
+++ src/Page/Tour/Tour.jsx
@@ -2,7 +2,9 @@
 const Tour = () => {
     return (
         <div>
-            
+           <div className="flex justify-center items-center text-5xl h-screen w-full">
+            Page is under maintenance
+        </div> 
         </div>
     );
 };
